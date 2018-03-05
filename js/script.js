1aa476fe7b428be7c1ffcb08b05ccc4bd682f103
Lucas <lucas@superko.org>
2018-03-05 00:35:31 -0500

    wip mobile menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-    
+    // Navigation
+    $('#menu-toggle').on('click tap', function() {
+        $('#header').toggleClass('opened');
+        $('#mobile-menu-content').toggleClass('opened');
+    });
 });
