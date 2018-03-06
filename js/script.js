a7901de5b718846a6675a2c901d26806763357a3
Lucas <lucas@superko.org>
2018-03-06 00:31:03 -0500

    menu scroll effect, some work on front page

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
         $('#header').toggleClass('opened');
         $('#mobile-menu-content').toggleClass('opened');
     });
+
+    // Window scroll effects
+    $(window).scroll(function() {
+        if (!$('#header').hasClass('scrolled') && $(this).scrollTop() >= 2) {
+            $('#header').addClass('scrolled');
+        }
+        if ($(this).scrollTop() < 2 && $('#header').hasClass('scrolled')) {
+            $('#header').removeClass('scrolled');
+        }
+    });
 });
