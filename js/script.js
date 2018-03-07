a3cb7e0f317360793f4ee49f2bc07b4decba021f
Lucas <lucas@superko.org>
2018-03-06 20:30:58 -0500

    front page img spacing. menu tap behav

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
         $('#mobile-menu-content').toggleClass('opened');
     });
 
+    $('.dropdown-toggle').on('tap', function() {
+        if (!$(this).parent('.menu-item').hasClass('opened')) {
+            $('.menu-item.opened').removeClass('opened');
+            $(this).parent('.menu-item').addClass('opened');
+        } else {
+            $('.menu-item.opened').removeClass('opened');
+        };
+
+    });
+
     // Window scroll effects
     $(window).scroll(function() {
         if (!$('#header').hasClass('scrolled') && $(this).scrollTop() >= 2) {
