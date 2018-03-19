0ae4d1ef84387132093812d3b7898de615d1d517
Lucas <lucas@superko.org>
2018-03-19 02:01:44 -0400

    sticky sidebar

--- _site/js/script.js
+++ _site/js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         };
     });
 
-    // Window scroll effects
+    // Add nav-header bottom border on scroll
     $(window).scroll(function() {
         if (!$('#header').hasClass('scrolled') && $(this).scrollTop() >= 2) {
             $('#header').addClass('scrolled');
@@ -23,4 +23,55 @@ $(document).ready(function() {
             $('#header').removeClass('scrolled');
         }
     });
+
+    // Smooth scroll on anchor click
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .on('click tap', function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top - 144 + 'px'
+            }, 1000, function() {
+              // Change focus?
+              // var $target = $(target);
+              // $target.focus();
+              // if ($target.is(":focus")) { // Checking if the target was focused
+              //   return false;
+              // } else {
+              //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              //   $target.focus(); // Set focus again
+              // };
+            });
+          }
+        }
+    });
+
+    // Sticky sidebar
+    if ($('.sidebar')) {
+        $(window).scroll(function() {
+            if (!$('.sidebar').hasClass('active')) {
+                if ($(window).scrollTop() - $('.sidebar-reference').offset().top + 240 > 0) {
+                    $('.sidebar').addClass('active');
+                }
+            } else {
+                if ($(window).scrollTop() - $('.sidebar-reference').offset().top + 240 < 0) {
+                    $('.sidebar').removeClass('active');
+                }
+            }
+
+        });
+    }
 });
